59063cf9641e17cc9c32e01f674ff86be5f7f8ea
Inoue Rion <135009547+moonxy57@users.noreply.github.com>
2023-08-11 15:22:53 +0900

    Update App.cpp

--- sgx-tmp/App/App.cpp
+++ sgx-tmp/App/App.cpp
@@ -6,28 +6,61 @@
 #include "error_print.hpp"
 #include <openssl/bio.h>
 
-sgx_enclave_id_t global_eid = 0;
+sgx_enclave_id_t eid = 0;
 
-
-/* display bytes of the secret buffer for debug */
-void ocall_debug_print(uint8_t *buf, size_t sz)
-{
-	//デバック標準出力用OCALL関数を定義
-}
-
+void ocall_print(const char* str)
+ {
+     std::cout << "Output from OCALL: " << std::endl;
+     std::cout << str << std::endl;
+     return;
+ }
 
 /* Enclave initialization function */
 int initialize_enclave()
 {
     //0埋めしたダミーの起動トークンでEnclaveを作成する
+	sgx_launch_token_t token = {0}; 
+
+	int update = 0;
+
+	std::string enclave_image_name = "enclave.signed.so";
+
+	sgx_status_t status;
+
+	status = sgx_create_enclave(enclave_image_name.c_str(), SGX_DEBUG_FLAG, &token, &update, &eid, NULL);
+
+	if (status != SGX_SUCCESS) {
+		printf("App: error %#x, failed to create enclave.\n", status);
+		return -1;
+	}
+
+	return 0;
 }
 
+int main(){
+	if(initialize_enclave() < 0)
+	{
+		std::cerr << "App: file error" << std::endl;
+	}
 
-int main()
-{
+
+	int retval = 0;
+	const char *message = "MESSAGE";
+	size_t message_len = strlen(message);
+	sgx_status_t status = ecall_tmp(eid, &retval, message, message_len);
+	
+	print_sgx_status(status);
+
+	std::cout << retval << std::endl;
+
+	sgx_destroy_enclave(eid);
+	return 0;
+}
+
 	/* 以下の処理を実装する：
 	 * - Enclaveの作成（初期化）
 	 * - ECALL関数の呼び出し
 	 * - ECALL結果のSGXステータス及び戻り値の出力
 	 */
-}
+	
+
